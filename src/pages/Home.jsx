afa4748619a1a869d8324b53e10320e8b55e7b0f
Derek <derek.baker89@gmail.com>
2022-06-27 14:04:41 +0100

    get user data from github api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import UserResults from "../components/users/UserResults";
 
 function Home(props) {
     return (
-        <div>
-            <h1 className="text-6xl">Welcome</h1>
-        </div>
+        <>
+            <UserResults />
+        </>
     );
 }
 
